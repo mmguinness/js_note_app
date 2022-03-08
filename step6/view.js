86c265da1544978e0b1d2fb3cac8ae4672d5fc47
Haroon <haroon20@hotmail.co.uk>
2022-03-08 17:23:32 +0000

    Step 7 complete

--- step6/view.js
+++ step6/view.js
@@ -1,15 +1,16 @@
 class View {
   constructor() {
     this.mainContainerEl = document.querySelector('#main-container');
-
     console.log(this.mainContainerEl);
-  }
+  };
 
   addParagraph() {
     let p = document.createElement("p");
-    this.mainContainerEl.append("Some new text", p);
-  }
+    p.innerText = "Some new text";
+    this.mainContainerEl.append(p);
+  };
 
-}
+};
 
 module.exports = View;
+
